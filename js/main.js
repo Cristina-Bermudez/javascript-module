70221bbac4255ee82ba9dd2d5722a6378aa06f11
Cristina-Bermudez <xmadden.lala@hotmail.com>
2019-09-18 18:04:03 -0500

    :octocat:js introduction

---
+++ js/main.js
@@ -0,0 +1,11 @@
+console.log('Hello World desde el src')
+
+var firstNumber = 10
+var secondNumber = 20
+var thirdNumber = 4
+
+(secondNumber % firstNumber) + (3 * ((firstNumber / 2) + 10));
+
+(firstNumber % 3 === 5 * (firstNumber * 2 + (3 + secondNumber)));
+
+(true && (100 > (((thirdNumber % 2) !== (3 * firstNumber)) * 3 < firstNumber + (secondNumber * 2))));
