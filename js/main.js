c917a8ee1d7131beef797a224c14ce27f669ea8d
Cristina-Bermudez <xmadden.lala@hotmail.com>
2019-09-19 01:44:11 -0500

    branch creation

--- js/main.js
+++
@@ -1,9 +0,0 @@
-var pet = 'parrot'
-
-if (pet === 'dog'){
-  console.log ('It is a dog');
-} else if (pet === 'parrot'){
-  console.log ('It is a parrot');
-}  else {
-  console.log ('It is another pet');
-}
