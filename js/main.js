a0ca050336d87d94abd5ffe886bd1cab0d1bbacd
Cristina-Bermudez <xmadden.lala@hotmail.com>
2019-09-21 22:59:34 -0500

    [METODOS DE STRING] .includes]

---
+++ js/main.js
@@ -0,0 +1,8 @@
+//EJERCICIO 1 --> Verificar si la palabra contiene las letras 'ing'
+var word = 'baloon';
+
+if (word .includes ( 'ing' )){
+  console.log ('La palabra contiene las letras "ing"')
+} else {
+    console.log ('La palabra no contiene las letras "ing"')
+}
